a8eac3b1e0298ba9421458fd1022a8f330152ad2
Alexandrbig1 <alexsmagin1@gmail.com>
2024-01-16 01:28:28 -0600

    feat: added icons for CTA buttons. added animation for icons

--- src/components/Cta/Cta.jsx
+++ src/components/Cta/Cta.jsx
@@ -1,10 +1,31 @@
-import { ButtonsContainer, HeroBtn } from "./Cta.styled";
+import {
+  ButtonsContainer,
+  HeroBtn,
+  ButtonIconTelWrapper,
+  ButtonIconMailWrapper,
+} from "./Cta.styled";
+import { FaPhone } from "react-icons/fa";
+import { GrMailOption } from "react-icons/gr";
 
 export default function Cta() {
   return (
     <ButtonsContainer>
-      <HeroBtn>Call Now</HeroBtn>
-      <HeroBtn>Email Me</HeroBtn>
+      <HeroBtn
+        href="tel:+17734141884"
+        aria-label="Phone number +1 (773) 4141884"
+        rel="noopener noreferrer"
+      >
+        Call Now
+        <ButtonIconTelWrapper>
+          <FaPhone />
+        </ButtonIconTelWrapper>
+      </HeroBtn>
+      <HeroBtn href="mailto:alexsmaginpt@gmail.com">
+        Email Me
+        <ButtonIconMailWrapper>
+          <GrMailOption />
+        </ButtonIconMailWrapper>
+      </HeroBtn>
     </ButtonsContainer>
   );
 }
--- src/components/Cta/Cta.styled.jsx
+++ src/components/Cta/Cta.styled.jsx
@@ -1,19 +1,111 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
+
+const buttonAnimation = keyframes`
+  0% {
+    transform: scale(1);
+  }
+  50% {
+    transform: scale(1.2);
+  }
+  100% {
+    transform: scale(1);
+  }
+`;
+
+const buttonEmailAnimation = keyframes`
+  0% {
+    transform: rotate(0deg) scale(1);
+  }
+  25% {
+    transform: rotate(-15deg) scale(1.2);
+  }
+  50% {
+    transform: rotate(15deg) scale(1.2);
+  }
+  75% {
+    transform: rotate(-15deg) scale(1.2);
+  }
+  100% {
+    transform: rotate(0deg) scale(1);
+  }
+`;
 
 export const ButtonsContainer = styled.div`
   display: flex;
   align-items: center;
-  gap: 2rem;
+  gap: 1.2rem;
   z-index: 10;
+
+  @media (min-width: 768px) {
+    display: flex;
+    align-items: center;
+    gap: 2rem;
+    z-index: 10;
+  }
 `;
 
-export const HeroBtn = styled.button`
+export const ButtonIconTelWrapper = styled.span`
+  color: ${(p) => p.theme.colors.greenColor};
+  font-size: 1.4rem;
+  width: 1.4rem;
+  height: 1.4rem;
+  transition: transform 5s ease-in-out;
+
+  @media (min-width: 768px) {
+    font-size: 2.4rem;
+    width: 2.4rem;
+    height: 2.4rem;
+  }
+`;
+
+export const ButtonIconMailWrapper = styled.span`
+  color: ${(p) => p.theme.colors.heroTextColor};
+  font-size: 1.4rem;
+  width: 1.4rem;
+  height: 1.4rem;
+  transition: transform 2s ease-in-out;
+
+  @media (min-width: 768px) {
+    font-size: 2.4rem;
+    width: 2.4rem;
+    height: 2.4rem;
+  }
+`;
+
+export const HeroBtn = styled.a`
+  width: 10.4rem;
   cursor: pointer;
   background-color: ${(p) => p.theme.colors.accentColor};
   outline: none;
   border: none;
-  padding: 1.2rem 2.4rem;
+  padding: 0.4rem 1.2rem;
   border-radius: 6px;
-  color: white;
+  color: ${(p) => p.theme.colors.heroTextColor};
   z-index: 20;
+  display: flex;
+  align-items: center;
+  gap: 0.8rem;
+  color: ${(p) => p.theme.colors.heroTextColor};
+  font-family: DM Sans;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 1.28;
+
+  &:hover {
+    ${ButtonIconTelWrapper} {
+      animation: ${buttonAnimation} 1.2s infinite ease-in-out;
+    }
+    ${ButtonIconMailWrapper} {
+      animation: ${buttonEmailAnimation} 2.5s infinite ease-in-out;
+    }
+  }
+
+  @media (min-width: 768px) {
+    width: auto;
+    gap: 1.2rem;
+    padding: 1.2rem 2.4rem;
+    font-size: 16px;
+    width: auto;
+  }
 `;
