18ce4de3258a5a27830c4f0aaa10c3e80a907d5e
Alexandrbig1 <alexsmagin1@gmail.com>
2024-01-23 11:27:24 -0600

    fix: updated more photos for slideimages component

--- src/components/SlideImages/SlideImages.jsx
+++ src/components/SlideImages/SlideImages.jsx
@@ -46,19 +46,19 @@ export default function SlideImages() {
           <SlideImage src="images/slide4.jpg" alt="bodybuilding competition" />
         </SlideImageWrapper>
         <SlideImageWrapper>
-          <SlideImage src="/images/slide5.jpg" alt="bodybuilding competition" />
+          <SlideImage src="images/slide5.jpg" alt="bodybuilding competition" />
         </SlideImageWrapper>
         <SlideImageWrapper>
-          <SlideImage src="/images/slide6.jpg" alt="bodybuilding" />
+          <SlideImage src="images/slide6.jpg" alt="bodybuilding" />
         </SlideImageWrapper>
         <SlideImageWrapper>
-          <SlideImage src="/images/slide7.jpg" alt="personal trainer" />
+          <SlideImage src="images/slide7.jpg" alt="personal trainer" />
         </SlideImageWrapper>
         <SlideImageWrapper>
-          <SlideImage src="/images/slide8.jpg" alt="bodybuilding" />
+          <SlideImage src="images/slide8.jpg" alt="bodybuilding" />
         </SlideImageWrapper>
         <SlideImageWrapper>
-          <SlideImage src="/images/slide9.jpg" alt="bodybuilding" />
+          <SlideImage src="images/slide9.jpg" alt="bodybuilding" />
         </SlideImageWrapper>
       </Slide>
     </SlideWrapper>
